1c7aaade7a9d12b049eff6c24c440cfbde054083
roll <roll@respect31.com>
2015-09-13 11:35:31 +0300

    added trial2method fk

--- archive/clean/sql/foreignkeys.sql
+++ archive/clean/sql/foreignkeys.sql
@@ -11,4 +11,9 @@ ALTER TABLE "trial2condition" ADD CONSTRAINT "trial2condition_trial_id_fk" FOREI
 ALTER TABLE "trial2condition" ADD CONSTRAINT "trial2condition_condition_id_fk" FOREIGN KEY ("condition_id") REFERENCES "condition"("id") ON UPDATE CASCADE ON DELETE CASCADE;
 
 
+-- Trial/Method
+ALTER TABLE "trial2method" ADD CONSTRAINT "trial2method_trial_id_fk" FOREIGN KEY ("trial_id") REFERENCES "trial"("id") ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE "trial2method" ADD CONSTRAINT "trial2method_method_id_fk" FOREIGN KEY ("method_id") REFERENCES "method"("id") ON UPDATE CASCADE ON DELETE CASCADE;
+
+
 END;

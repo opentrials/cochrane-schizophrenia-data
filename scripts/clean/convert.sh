67ba3fff34c3d270b79a37328ef5ba88f00eb57e
roll <roll@respect31.com>
2015-09-16 17:37:29 +0300

    added schema to datapackage.json

--- scripts/clean/convert.sh
+++ scripts/clean/convert.sh
@@ -7,7 +7,7 @@ set -x
 for sqlfile in $(ls archive/clean/sql/data-*); do
     table=$(basename $sqlfile ".sql")
     table=${table:5}
-    csvfile="archive/clean/csv/${table}.csv"
+    csvfile="archive/clean/csv/data/${table}.csv"
     query="\\copy \"clean\".\"${table}\" To '${csvfile}' With CSV HEADER"
     psql -p 15431 -h localhost -U postgres -d postgres -c "${query}"
 done
